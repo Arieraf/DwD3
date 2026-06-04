bd89626fec8c2b5b3719d879d7c507996e172f3c
Tian F. <fereira42u@gmail.com>
2026-06-04 15:18:27 +0700

    second

--- GB.js
+++ GB.js
@@ -1,6 +1,39 @@
-const btn = document.getElementById("menuBtn");
-const menu = document.getElementById("menu");
+const form = document.getElementById("myForm");
+const hasil = document.getElementById("hasil");
 
-btn.addEventListener("click",function() {
-    menu.classList.toggle("show");
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    const nama = document.getElementById("nama").value;
+    const pesan = document.getElementById("pesan").value;
+
+    const item = document.createElement("li");
+
+    item.classList.add("list-group-item");
+
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+
+   
+    const strong = document.createElement("strong");  
+    strong.innerHTML = `<i class="bi bi-person-fill"></i> ${nama}`;
+
+    const p = document.createElement("p");
+    p.innerHTML = `${pesan}`;
+
+    li.appendChild(strong);  
+    li.appendChild(document.createElement("br"));
+    li.appendChild(p);
+
+    hasil.appendChild(li);  
+
+    form.reset();
+
 });
+
+
+
+
+
+
